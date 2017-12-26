88357151cecd25e6db309022378e27fd8da8cd62
sunny-udhani <sunny.iv09@gmail.com>
2017-12-26 01:20:25 -0800

    api changes

--- kayak_kafka_frontend/app.js
+++ kayak_kafka_frontend/app.js
@@ -54,9 +54,19 @@ app.use(expressSessions({
 app.use(passport.initialize());
 
 app.use(express.static(path.join(__dirname,'client/build')));
+
 app.get('*', (req,res) => {
 	res.sendFile(path.join(__dirname+ 'client/build/index.html' ));
 });
+
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/admin', admin);
+app.use('/listings', listings);
+app.use('/listingDetail', listingDetail);
+app.use('/logger', infoLogger);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   let err = new Error('Not Found');
